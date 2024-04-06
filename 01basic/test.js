e1e0ffe6223645382937f326d6adbff50baf1e47
Satnam Marday <83498275+toolsmeen@users.noreply.github.com>
2024-04-06 10:42:22 +0000

    1st file done

--- 01basic/test.js
+++ 01basic/test.js
@@ -1,1 +1,8 @@
-console.log("Satnam")
+
+const username = "admin";
+let fullname = "Satnam Singh"
+address = "Rampura Gujran"
+
+fullname = "Sahil Singh Marday"
+address= "Tohana"
+console.table([username, fullname,address]);
